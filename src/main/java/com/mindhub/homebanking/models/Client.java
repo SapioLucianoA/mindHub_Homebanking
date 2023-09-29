e4319c9a21e070d2913265fea64cf20a85821714
Sapio Luciano <sapioluciano99@gmail.com>
2023-09-29 02:11:24 -0300

    prueba de rama sprint-1

---
+++ src/main/java/com/mindhub/homebanking/models/Client.java
@@ -0,0 +1,70 @@
+package com.mindhub.homebanking.models;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+
+    private Long id;
+    private String name;
+    
+    private String lastName;
+    
+    private String mail;
+
+
+    public Client() {
+    }
+
+    public Client(String name, String lastName, String mail) {
+        this.name = name;
+        this.lastName = lastName;
+        this.mail = mail;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", mail='" + mail + '\'' +
+                '}';
+    }
+}
